004def0eea290b916649c7710233e4fe8af71db0
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-09-21 18:40:10 +0200

    popup

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -77,6 +77,85 @@ const popupDefaultOptions={
   },
 }
 
-// setTimeout(function () {
-//   launchPopup('link1');
-// }, 2000);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////POPUP IMATGES//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const launchImagePopup = async (imageURL, width=null, height=null, providedOptions = {}) => {
+  const options = { ...popupDefaultOptions, ...providedOptions };
+console.log(imageURL);
+console.log(width);
+  const overlay = document.createElement("div");
+  overlay.className = "iframeOverlay";
+  if (options.overlayColor) {
+      overlay.style.backgroundColor = options.overlayColor;
+  }
+
+  const imageContainer = document.createElement("div");
+  imageContainer.className = "iframeContainer";
+
+  const imgLoader = document.createElement("div");
+  imgLoader.className = "iframeLoader";
+  imageContainer.appendChild(imgLoader);
+
+  const img = document.createElement("img");
+  img.src = imageURL;
+  img.style.opacity = "0";
+  img.style.transition = "opacity 0.5s";
+
+  img.onload = function () {
+      imgLoader.style.display = "none";
+      imageContainer.style.opacity = "1";
+      img.style.opacity = "1";
+      img.style.width = "100%";
+
+      // Si es proporciona l'ample però no l'alçada, calculem l'alçada proporcional
+      if (width && !height) {
+        height = `${img.naturalHeight * (parseInt(width) / img.naturalWidth)}px`;
+        console.log('height', height);
+      }
+
+      // Si es proporciona l'alçada però no l'ample, calculem l'ample proporcional
+      if (!width && height) {
+          width = `${img.naturalWidth * (parseInt(height) / img.naturalHeight)}px`;
+          console.log('width', width);
+        }
+
+
+      // Si no es proporciona ni l'ample ni l'alçada, utilitzem les dimensions originals de la imatge
+      if (!width && !height) {
+          width = `${img.naturalWidth}px`;
+          height = `${img.naturalHeight}px`;
+          console.log('uuuuuuuuuuuuuultim');
+          console.log('width', width);
+          console.log('height', height);
+        }
+
+      imageContainer.style.width = width;
+      // imageContainer.style.height = height;
+      
+      
+  };
+
+  imageContainer.appendChild(img);
+  overlay.appendChild(imageContainer);
+
+  const closeButton = document.createElement("button");
+  closeButton.innerText = "X";
+  closeButton.className = "iframeCloseButton";
+  closeButton.addEventListener("click", () => closePopup(overlay, imageContainer, options));
+  imageContainer.appendChild(closeButton);
+
+  overlay.addEventListener("click", function (event) {
+      if (event.target === overlay) {
+          closePopup(overlay, imageContainer, options);
+      }
+  });
+
+  document.body.appendChild(overlay);
+  img.onerror = function (error) {
+    console.error("Hi ha hagut un error en carregar la imatge:", error);
+};
+  overlay.style.opacity = "1";
+  await awaitStylecomplete(overlay, "opacity", "1");
+}
